c52e0ef651031598f61672f2617fa29d79987671
matt manes <mattmanes@pm.me>
2023-03-31 12:23:37 -0500

    refactor: remove uneccessary git command in commit_all block

--- src/hassle/hassle.py
+++ src/hassle/hassle.py
@@ -190,8 +190,7 @@ def main(args: argparse.Namespace = None):
         if args.commit_all == "build":
             version = tomlkit.loads(pyproject_path.read_text())["project"]["version"]
             args.commit_all = f"chore: build v{version}"
-        os.system(f"git add .")
-        os.system(f'git commit -a -m "{args.commit_all}"')
+        os.system(f'git commit -m "{args.commit_all}"')
 
     if args.tag_version:
         hassle_utilities.tag_version(args.package)
