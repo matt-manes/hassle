69e4dc094b4c40416651c858ae545669d134ee64
matt manes <mattmanes@pm.me>
2023-04-07 17:46:47 -0500

    feat: generate_tests cli accepts individual files instead of only directories

--- src/hassle/generate_tests.py
+++ src/hassle/generate_tests.py
@@ -1,31 +1,31 @@
 import argparse
 import os
 import tokenize
-from pathlib import Path
+from pathier import Pathier
 
-root = Path(__file__).parent
+root = Pathier(__file__).parent
 
 
 def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "package_name",
+        "paths",
         type=str,
         default=".",
-        nargs="?",
+        nargs="*",
         help=""" The name of the package or project to generate tests for,
         assuming it's a subfolder of your current working directory.
         Can also be a full path to the package. If nothing is given,
-        the current working directory will be used.""",
+        the current working directory will be used.
+        Can also be individual files.""",
     )
 
     args = parser.parse_args()
-
     return args
 
 
-def get_function_names(filepath: Path) -> list[str]:
+def get_function_names(filepath: Pathier) -> list[str]:
     """Returns a list of function names from a .py file."""
     with filepath.open("r") as file:
         tokens = list(tokenize.generate_tokens(file.readline))
@@ -41,7 +41,9 @@ def get_function_names(filepath: Path) -> list[str]:
     return functions
 
 
-def write_placeholders(package_path: Path, pyfile: Path | str, functions: list[str]):
+def write_placeholders(
+    package_path: Pathier, pyfile: Pathier | str, functions: list[str]
+):
     """Write placeholder functions to the
     tests/test_{pyfile} file if they don't already exist.
     The placeholder functions use the naming convention
@@ -56,7 +58,7 @@ def write_placeholders(package_path: Path, pyfile: Path | str, functions: list[s
     package_name = package_path.stem
     tests_dir = package_path / "tests"
     tests_dir.mkdir(parents=True, exist_ok=True)
-    pyfile = Path(pyfile)
+    pyfile = Pathier(pyfile)
     test_file = tests_dir / f"test_{pyfile.name}"
     # Makes sure not to overwrite previously written tests
     # or additional imports.
@@ -73,7 +75,7 @@ def write_placeholders(package_path: Path, pyfile: Path | str, functions: list[s
     os.system(f"isort {tests_dir}")
 
 
-def generate_test_files(package_path: Path):
+def generate_test_files(package_path: Pathier):
     """Generate test files for all .py files in 'src'
     directory of 'package_path'."""
     pyfiles = [
@@ -88,8 +90,12 @@ def generate_test_files(package_path: Path):
 def main(args: argparse.Namespace = None):
     if not args:
         args = get_args()
-    package_path = Path(args.package_name).resolve()
-    generate_test_files(package_path)
+    args.paths = [Pathier(path).resolve() for path in args.paths]
+    for path in args.paths:
+        if path.is_dir():
+            generate_test_files(path)
+        elif path.is_file():
+            write_placeholders(path.parent, path, get_function_names(path))
 
 
 if __name__ == "__main__":
