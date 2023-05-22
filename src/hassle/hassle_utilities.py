6d948fb854d65f62ab807ce9d2556fe17d680e22
matt manes <mattmanes@pm.me>
2023-05-21 20:07:25 -0500

    perf: add filters for changelog generation

--- src/hassle/hassle_utilities.py
+++ src/hassle/hassle_utilities.py
@@ -139,7 +139,13 @@ def update_changelog(pyproject_path: Pathier):
             stdout=subprocess.PIPE,
             text=True,
         ).stdout.splitlines(True)
-        if not line.startswith("Full set of changes:")
+        if not line.startswith(
+            [
+                "Full set of changes:",
+                f"* build {config['git']['tag_prefix']}",
+                "* update changelog",
+            ]
+        )
     ]
     if changelog_path.exists():
         previous_changelog = changelog_path.read_text().splitlines(True)[
